bbfed98607a5f6e81ca94ffc4536514a59f02999
Pity Sentient beings <118274136+illusionarybookrevelation@users.noreply.github.com>
2023-11-21 16:08:19 +0800

    ...

--- 函数初步.py
+++ 函数初步.py
@@ -10,3 +10,42 @@ def func3():
     i=7
 func3()
 print(i)
+
+#LEGB法则
+range=8
+def func1():
+    ranne=9
+    def func2():
+        range=6
+        print(range)
+    func2()
+func1()
+
+#单个返回值
+def test():
+    i=7
+    i+=1
+    return i
+print(test())
+
+#多个返回值
+def funcrtn(i,j):
+    k=i*j
+    return (i,j,k)
+x=funcrtn(4,5)#返回值第一种接受方式
+print(x)
+a,b,c=funcrtn(7,8)#返回值第二种接受方式
+print(a)
+print(b)
+print(c)
+
+#文档字符串
+def d(i,j):
+    '''这个函数实现了一个乘法运算
+
+
+    函数会返回一个乘法运算的结果'''
+    k=i*j
+    return k
+print(d.__doc__)   #第一种调用方法
+print(help(d)) #第二种调用方法
---
+++ 模块.py
@@ -0,0 +1,6 @@
+import first.abc
+print(first.add(3,5))
+
+print(first.abc.sub(10,5))
+
+print(dir(first))
